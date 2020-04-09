17625639838d8f57143537d2e7a2b8044f4d76cf
ethanzohar <ethanzohar9@gmail.com>
2020-04-09 19:05:16 -0400

    changing endpoint

--- frontend/src/Listen.js
+++ frontend/src/Listen.js
@@ -74,7 +74,7 @@ const handleClick = (e) => {
 }
 
 async function get() {
-  const response = await fetch('http://localhost:8080/spotify/listener/streamers', {'Accepts': 'application/json'})
+  const response = await fetch('http://ethanzohar.com:8080/spotify/listener/streamers', {'Accepts': 'application/json'})
   if (response && response.ok && response.body) {
     var streamers = await response.json();
 
