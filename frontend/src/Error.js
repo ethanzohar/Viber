9d66572dcf41aae13e4dbb95bd67b70bd950cbb8
ethanzohar <ethanzohar9@gmail.com>
2020-04-09 19:23:35 -0400

    trying for alex

--- frontend/src/Error.js
+++ frontend/src/Error.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const Error = () => {
+const Error = async () => {
+
+  const getHandler = async () => {
+    const response = await fetch('http://localhost:8080/endpoints', {'Accepts': 'application/json'})
+    if (response && response.ok && response.body) {
+      return await response.json();
+    } else {
+      return "dumb"
+    }
+  }
 
   return (
-    <div className="Error-Page">
-      <h1>An Error occurred LMAO</h1>
-    </div>
+    await getHandler()
   );
+
+  // return (
+  //   <div className="Error-Page">
+  //     <h1>An Error occurred LMAO</h1>
+  //   </div>
+  // );
 }
 
 export default Error;
